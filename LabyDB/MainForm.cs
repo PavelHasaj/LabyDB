d804fa0e707265e6dcec3466eb252586c837c361
Павел Хасай <hasajpavel@gmail.com>
2021-11-26 09:04:30 +0300

    Добавлен файл TODO, выполнена очистка кода, переименована начальная форма

---
+++ LabyDB/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LabyDB {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void Form2_open_button(object sender, EventArgs e) {
+            Program.form2.Show();
+            this.Hide();
+        }
+
+        private void Form3_open_button(object sender, EventArgs e)
+        {
+            Program.form3.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Program.form4.Show();
+            this.Hide();
+        }
+    }
+}
